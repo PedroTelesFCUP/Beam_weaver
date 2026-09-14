6596ea389d707aaeaf94e4f6bd243799dc8147c0
Pedro Teles <36714567+PedroTelesFCUP@users.noreply.github.com>
2026-09-14 04:09:16 +0100

    Update transport.py

--- beamweaver/transport.py
+++ beamweaver/transport.py
@@ -1,6 +1,6 @@
 """Beam Weaver: transport."""
 
-# Copyright (c) 2025–2026 Pedro Teles & João Melo. Apache-2.0.
+# Copyright (c) 2025–2026 Pedro Teles. Apache-2.0.
 
 from .constants import PAIR_THRESHOLD_MEV
 from .constants import (
